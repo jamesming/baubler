0f6af9b139f405d3225445b10392b77329493204
James Ming <jamesming@gmail.com>
2012-09-04 22:08:39 -0700

    added

--- _source/pull.php
+++ _source/pull.php
@@ -1,7 +1,6 @@
 <?php
 
-$output = shell_exec('/home/jamesming/bin/git pull.php');
-$output = shell_exec('ls -l /home/jamesming/bin/');
+$output = shell_exec('/home/jamesming/bin/git pull');
  echo "<pre>$output</pre>";
 
 ?>
